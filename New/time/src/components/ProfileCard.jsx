c30d7f51b8a7a51e193b9a31e5fd430554897141
Your Name <you@example.com>
2025-05-13 20:12:42 +0630

    fix

--- New/time/src/components/ProfileCard.jsx
+++ New/time/src/components/ProfileCard.jsx
@@ -3,34 +3,33 @@ import React from 'react'
 function ProfileCard() {
     return (
         <>
-          <div className="bg-Navy-900 w-full md:max-w-[300px] min-h-[523px]  relative rounded-2xl overflow-hidden">
-              <div className="bg-Purple-600 min-h-[65%] w-full absolute rounded-xl p-8">
-                   <div className="">
-                       <div>
-                           <img src="/images/image-jeremy.png" alt="profile" className="max-w-[60px] outline-white outline-3 rounded-full "/>
-                       </div>
-                      <div className="mt-8">
-                          <p className="text-Navy-200">
-                              Report for
-                          </p>
-                           <h2 className="text-white text-4xl font-light">
-                               Jeremy Robson
-                           </h2>
-                      </div>
-                   </div>
-              </div>
+            <div className="bg-Navy-900 w-full max-w-[280px] mx-auto min-h-[180px] md:max-w-[300px] md:min-h-[523px] relative rounded-2xl overflow-hidden">
 
-              <div className="absolute bottom-0 p-8 grid grid-cols-1 gap-5 ">
-                      <p className="text-Navy-200">
-                          Daily
-                      </p>
-                      <p className="text-white  font-medium text-[18px]">
-                          Weekly
-                      </p>
-                      <p className="text-Navy-200">Monthly</p>
+                <div className="bg-Purple-600 flex lg:flex-col lg:items-start items-center h-[120px] p-4 md:absolute md:top-0 md:left-0 md:w-full md:min-h-[65%] md:h-auto md:rounded-xl md:p-8">
 
-              </div>
-          </div>
+                    <img src="/images/image-jeremy.png" alt="profile" className="w-12 h-12 rounded-full outline-white outline-3 md:max-w-[60px] md:w-auto md:h-auto"/>
+
+                    <div className="ml-4 md:ml-0 md:mt-8">
+                        <p className="text-Navy-200 text-sm md:text-base">
+                            Report for
+                        </p>
+                        <h2 className="text-white text-lg md:text-4xl font-light">
+                            Jeremy Robson
+                        </h2>
+                    </div>
+                </div>
+
+
+                <div className="absolute bottom-0 w-full flex justify-between p-6 md:grid md:grid-cols-1 md:gap-5 md:p-8 ">
+
+                    <p className="text-Navy-200 text-sm md:text-base">Daily</p>
+                    <p className="text-white font-medium text-sm md:text-[18px]">
+                        Weekly
+                    </p>
+                    <p className="text-Navy-200 text-sm md:text-base">Monthly</p>
+
+                </div>
+            </div>
         </>
     )
 }
--- New/time/src/components/TimeCard.jsx
+++ New/time/src/components/TimeCard.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 function TimeCard({time, image, cardTitle, subTime, bgColor}) {
     return (
         <>
-            <div className={`${bgColor} relative w-full md:max-w-[300px] min-h-[250px]  rounded-xl overflow-hidden`}>
+            <div className={`${bgColor} relative w-full md:max-w-[300px] min-h-[170px] md:min-h-[250px]  rounded-xl overflow-hidden`}>
                 <img src={image} alt="icon" className="absolute right-4"/>
-             <div className="bg-Navy-900 absolute w-full min-h-[80%] bottom-0  rounded-t-xl p-7 flex flex-wrap flex-col justify-between">
+             <div className="bg-Navy-900 absolute w-full min-h-[70%] md:min-h-[80%] bottom-0  rounded-t-xl p-7 flex flex-wrap flex-col justify-between">
                  <div className="flex justify-between items-center">
                      <h5 className="text-white font-medium text-[18px] ">{cardTitle}</h5>
                      <img src="/images/icon-ellipsis.svg" alt="icon"/>
                  </div>
-                 <div className="">
+                 <div className="flex md:block justify-between items-center">
                      <h2 className="text-white font-light text-5xl">
                          {time}
                      </h2>
